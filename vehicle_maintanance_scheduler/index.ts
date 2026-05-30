9cbd91d8d6a1fb257e200031952b8bf3ca445e99
sashi-does <sashiontop@gmail.com>
2026-05-30 13:08:45 +0530

    #Vehicle scheduler on score, time

--- vehicle_maintanance_scheduler/index.ts
+++ vehicle_maintanance_scheduler/index.ts
@@ -46,31 +46,58 @@ export async function getTasks(
             });
 
             // sorted lists 
-            console.log("######")
-            console.log(response.data)
-            console.log("######")
+            // console.log("######")
+            // console.log(response.data)
+            // console.log("######")
 
-            let data = response.data;
-
-            let lists = data.vehicles;
-
-            // Array.prototype.sort(lists, (a, b) => {
+             // Array.prototype.sort(lists, (a, b) => {
             //     if (a.impact / a.duration > b.impact / b.duration) return -1;
             //     if (a.impact / a.duration < b.impact / b.duration) return 1;
             //     return 0;
             // });
 
-            let final_list = [];
-            // let curr_time = 0;
-            for (i = 0; i < lists.length; i++) {
-                if (lists[i].Duration <= time && lists[i].Impact >= score) {
-                    final_list.push(lists[i]);
-                    // curr_time += lists[i].Duration;
+            // let final_list = [];
+            // // let curr_time = 0;
+            // for (i = 0; i < lists.length; i++) {
+            //     if (lists[i].Duration <= time && lists[i].Impact >= score) {
+            //         final_list.push(lists[i]);
+            //         // curr_time += lists[i].Duration;
+            //     }
+            // }
+            // console.log("_____")
+            // console.log(final_list.vehicles)
+            // console.log("_____")
+
+
+            const data = response.data;
+            const lists = data.vehicles;
+
+            lists.sort((a, b) => {
+                const scoreA = (Number(a.impact)) / (Number(a.duration));
+                const scoreB = (Number(b.impact)) / (Number(b.duration));
+                if (scoreA > scoreB) return -1;
+                if (scoreA < scoreB) return 1;
+                return 0;
+            });
+
+            const final_list = [];
+
+            let curr_time = 0;
+            for (let i = 0; i < lists.length; i++) {
+                const item = lists[i];
+                const duration = Number(item.duration ?? item.Duration ?? 0);
+                const impact = Number(item.impact ?? item.Impact ?? 0);
+                if (duration <= 0) continue;
+
+                if (impact >= score && curr_time + duration <= time) {
+                    final_list.push(item);
+                    curr_time += duration;
                 }
             }
-            console.log("_____")
-            console.log(final_list.vehicles)
-            console.log("_____")
+            console.log("Time: 5\n Score: 8\n")
+            console.log("\nSelected Tasks:\n", final_list, "\n");
+
+            return final_list;
 
         } catch (error) {
             if (axios.isAxiosError(error) && error.response?.status === 401) {
@@ -101,4 +128,4 @@ export async function getTasks(
 
 
 
-console.log(getTasks(5, 8));
+getTasks(5, 8);
